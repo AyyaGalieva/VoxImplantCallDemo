f8f174b4b60d562bcaf7189774d691e5e702ca91
Ayya <a.galieva@emergingtravel.com>
2024-07-26 10:49:41 +0100

    Initial Commit

---
+++ VoxImplantCallDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoxImplantCallDemo
+//
+//  Created by Ayya Galieva on 26.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
